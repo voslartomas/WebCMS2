44b0b6ae63dc963d897e9117d39dc9ffb200594c
Tomáš Voslař <tom@localhost.localdomain>
2013-08-29 00:26:09 +0200

    added version to template

--- AdminModule/presenters/BasePresenter.php
+++ AdminModule/presenters/BasePresenter.php
@@ -22,6 +22,8 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter{
 		}
 		
 		$this->setLayout("layout");
+		
+		$this->template->version = file_get_contents('../libs/webcms2/webcms2/AdminModule/version');
 	}
 	
 	/* Startup method. */
