38ce2a1fb11583ee9bc77eafc7e4537099dce460
Tomáš Voslař <tom@localhost.localdomain>
2013-08-31 02:13:33 +0200

    added tab into languages, grido settings

--- AdminModule/presenters/LanguagesPresenter.php
+++ AdminModule/presenters/LanguagesPresenter.php
@@ -28,8 +28,23 @@ class LanguagesPresenter extends \AdminModule\BasePresenter{
 		
 		$grid = new \Grido\Grid($this, $name);
 		$grid->setModel($qb->select('l')->from('AdminModule\Language', 'l'));
-		$grid->addColumn('name', 'Název');
+		$grid->addColumn('name', 'Název')->setSortable();
+		$grid->addColumnText('abbr', "Zkratka")->setSortable();
+		$grid->addColumn('default', "Výchozí")->setReplacement(array(
+			'1' => 'Ano',
+			NULL => 'Ne'
+		));
+		
+		$grid->addFilter('name', "Název");
+		$grid->addAction("edit", "Upravit")->getElementPrototype()->addAttributes(array('class' => 'btn btn-primary ajax'));
+		$grid->addAction("delete", "Smazat")->getElementPrototype()->addAttributes(array('class' => 'btn btn-danger ajax'));
+		$grid->setRememberState();
+		$grid->setExporting();
 		
 		return $grid;
+	}
+	
+	public function renderTranslates(){
+		
 	}
 }
