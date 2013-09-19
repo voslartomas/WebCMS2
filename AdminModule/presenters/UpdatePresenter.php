5947f7f7853bc71c15ea1a4a27bff3fe981994fc
t.voslar@gmail.com <t.voslar@gmail.com>
2013-09-19 14:39:29 +0200

    fix

--- AdminModule/presenters/UpdatePresenter.php
+++ AdminModule/presenters/UpdatePresenter.php
@@ -26,20 +26,22 @@ class UpdatePresenter extends \AdminModule\BasePresenter{
 		
 		putenv("COMPOSER_HOME=/usr/bin/.composer");
 		
-		system("cd ../;composer update > ../log/install.log 2> ../log/install-error.log");
-		$res = file_get_contents('../log/install.log');
+		system("cd ../;composer update > ./log/install.log 2> ./log/install-error.log");
+		$res = file_get_contents('./log/install.log');
 		
 		$this->flashMessage($res, 'success');
-		if(file_exists('../log/install-error.log')){
-			$resError = file_get_contents('../log/install-error.log');
+		if(file_exists('./log/install-error.log')){
+			$resError = file_get_contents('./log/install-error.log');
 			
 			if(!empty($resError)) 
 				$this->flashMessage($resError, 'danger');
 			
-			unlink('../log/install-error.log');
+			unlink('./log/install-error.log');
 		}
 				
-		unlink('../log/install.log');
+		if(file_exists('./log/install.log')){ 
+			unlink('./log/install.log');
+		}
 		
 		if(!$this->isAjax())
 			$this->redirect('Update:');
