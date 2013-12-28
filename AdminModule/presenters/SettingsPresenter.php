5e5a5d47cf1e295e083cf2827e2e44ac8bf07951
Tomáš Voslař <t.voslar@gmail.com>
2013-12-28 01:27:45 +0100

    added bing and google api keys to settings

--- AdminModule/presenters/SettingsPresenter.php
+++ AdminModule/presenters/SettingsPresenter.php
@@ -352,6 +352,11 @@ class SettingsPresenter extends \AdminModule\BasePresenter{
 		$settings = array();
 		$settings[] = $this->settings->get('Yandex API key', \WebCMS\Settings::SECTION_BASIC, 'text');
 		
+		$settings[] = $this->settings->get('Google API key', \WebCMS\Settings::SECTION_BASIC, 'text');
+		
+		$settings[] = $this->settings->get('Bing client id', \WebCMS\Settings::SECTION_BASIC, 'text');
+		$settings[] = $this->settings->get('Bing client secret', \WebCMS\Settings::SECTION_BASIC, 'text');
+		
 		return $this->createSettingsForm($settings);
 	}
 }
