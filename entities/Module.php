4141dbf724691cac4775e355cd52d9c2eb39dbad
Tomáš Voslař <t.voslar@gmail.com>
2014-01-30 22:06:08 +0000

    fixed classpath, moved entities directory

---
+++ entities/Module.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AdminModule;
+
+use Doctrine\ORM\Mapping as orm;
+
+/**
+ * Description of Module entity.
+ * @orm\Entity
+ * @author Tomáš Voslař <tomas.voslar at webcook.cz>
+ */
+class Module extends \AdminModule\Doctrine\Entity{
+	/**
+	 * @orm\Column
+	 */
+	private $name;
+	
+	/**
+	 * @orm\Column(type="text")
+	 */
+	private $presenters;
+	
+	/**
+	 * @orm\Column(type="boolean")
+	 */
+	private $active;
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	public function getActive() {
+		return $this->active;
+	}
+
+	public function setActive($active) {
+		$this->active = $active;
+	}
+	
+	public function getPresenters() {
+		return unserialize($this->presenters);
+	}
+
+	public function setPresenters($presenters) {
+		$this->presenters = serialize($presenters);
+	}
+}
---
+++ entities/Thumbnail.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace AdminModule;
+
+use Doctrine\ORM\Mapping as orm;
+
+/**
+ * Description of Thumbnail
+ * @orm\Entity
+ * @author Tomáš Voslař <tomas.voslar at webcook.cz>
+ */
+class Thumbnail extends \AdminModule\Doctrine\Entity{
+	/**
+	 * @ORM\Column(name="`key`")
+	 * @var String
+	 */
+	private $key;
+	
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var Int
+	 */
+	private $x;
+	
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var Int 
+	 */
+	private $y;
+	
+	/**
+	 * @ORM\Column(type="boolean")
+	 * @var Boolean 
+	 */
+	private $watermark;
+	
+	/**
+	 * @ORM\Column(type="boolean")
+	 * @var Boolean 
+	 */
+	private $system;
+        
+        /**
+         * @ORM\Column(type="integer")
+         * @var Int
+         */
+        private $resize;
+
+        /**
+         * @ORM\Column(type="boolean")
+         * @var 
+         */
+        private $crop;
+        
+        public function getCrop() {
+            return $this->crop;
+        }
+
+        public function setCrop($crop) {
+            $this->crop = $crop;
+        }
+        
+        public function getResize() {
+            return $this->resize;
+        }
+
+        public function setResize($resize) {
+            $this->resize = $resize;
+        }
+        
+	public function getKey() {
+		return $this->key;
+	}
+
+	public function setKey($key) {
+		$this->key = $key;
+	}
+
+	public function getX() {
+		return $this->x == 0 ? NULL : $this->x;
+	}
+
+	public function setX($x) {
+		$this->x = $x;
+	}
+
+	public function getY() {
+		return $this->y == 0 ? NULL : $this->y;
+	}
+
+	public function setY($y) {
+		$this->y = $y;
+	}
+
+	public function getWatermark() {
+		return $this->watermark;
+	}
+
+	public function setWatermark($watermark) {
+		$this->watermark = $watermark;
+	}
+
+	public function getSystem() {
+		return $this->system;
+	}
+
+	public function setSystem($system) {
+		$this->system = $system;
+	}
+}
