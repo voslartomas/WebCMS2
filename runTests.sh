84e761c4c112eabbe88d22f2d5463696cd3e70ba
Tomas Voslar <tomas.voslar@webcook.cz>
2014-07-12 15:25:21 +0100

    Added code coverage.

--- runTests.sh
+++ runTests.sh
@@ -4,3 +4,11 @@ mkdir tests/temp
 mkdir tests/log
 mkdir upload
 mkdir thumbnails
+
+if [ "$1" = "" ]; then
+    exit 0
+fi
+
+phpunit --coverage-clover=coverage.clover tests
+wget https://scrutinizer-ci.com/ocular.phar
+php ocular.phar code-coverage:upload --format=php-clover coverage.clover
