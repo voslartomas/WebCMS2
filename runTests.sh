ea8afaca9c64861b9e1aba64615700d91d199a57
Tomas Voslar <tomas.voslar@webcook.cz>
2014-07-12 14:28:40 +0100

    Updated travis file.

--- runTests.sh
+++ runTests.sh
@@ -4,7 +4,3 @@ mkdir tests/temp
 mkdir tests/log
 mkdir upload
 mkdir thumbnails
-
-phpunit --no-globals-backup --bootstrap tests/bootstrap.php tests/AdminModule
-phpunit --no-globals-backup --bootstrap tests/bootstrap.php tests/Entity
-phpunit --no-globals-backup --bootstrap tests/bootstrap.php tests/libs
