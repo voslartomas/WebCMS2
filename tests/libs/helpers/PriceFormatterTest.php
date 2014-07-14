9930c5f8f457a202072f6b9d4c707bb235435218
Tomas Voslar <tomas.voslar@webcook.cz>
2014-07-14 21:58:02 +0100

    Fixed formatter.

--- tests/libs/helpers/PriceFormatterTest.php
+++ tests/libs/helpers/PriceFormatterTest.php
@@ -8,6 +8,6 @@ class PriceFormatterTest extends \WebCMS\Tests\EntityTestCase
         \WebCMS\Helpers\PriceFormatter::setLocale($locales[0]);
         $price = \WebCMS\Helpers\PriceFormatter::format(1000);
 
-        $this->assertEquals('€1,000.00', $price);
+        $this->assertEquals('$1,000.00', $price);
     }       
 }
