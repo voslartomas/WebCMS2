ad75ec35714ad27cea9840c663ae5337fddee353
Tomáš Voslař <tomas.voslar@webcook.cz>
2014-07-18 16:20:52 +0100

    Update BasePresenter.php

--- Common/BasePresenter.php
+++ Common/BasePresenter.php
@@ -4,6 +4,8 @@ namespace WebCMS2\Common;
 
 abstract class BasePresenter extends \Nette\Application\UI\Presenter
 {
+	protected $em;
+	
 	protected abstract function getLanguageId();
 
 	protected function startUp()
