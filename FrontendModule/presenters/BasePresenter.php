c07919481ec9341a03d50de5ff8b70e2b2b5f819
Tomáš Voslař <tomas.voslar@webcook.cz>
2014-08-12 18:10:05 +0100

    Added custom layout path.

--- FrontendModule/presenters/BasePresenter.php
+++ FrontendModule/presenters/BasePresenter.php
@@ -517,5 +517,31 @@ class BasePresenter extends \WebCMS2\Common\BasePresenter
             "$dir/templates/$presenter.$this->view.phtml",
         );
     }
+    
+     /**
+      * Formats layout template file names.
+      * @return array
+      */
+     public function formatLayoutTemplateFiles()
+     {
+         $name = $this->getName();
+         $presenter = substr($name, strrpos(':' . $name, ':'));
+         $layout = $this->layout ? $this->layout : 'layout';
+         $dir = dirname($this->getReflection()->getFileName());
+         $dir = is_dir("$dir/templates") ? $dir : dirname($dir);
+         $list = array(
+             APP_DIR . "/templates/@$layout.latte",
+             "$dir/templates/$presenter/@$layout.latte",
+             "$dir/templates/$presenter.@$layout.latte",
+             "$dir/templates/$presenter/@$layout.phtml",
+             "$dir/templates/$presenter.@$layout.phtml",
+         );
+         do {
+             $list[] = "$dir/templates/@$layout.latte";
+             $list[] = "$dir/templates/@$layout.phtml";
+             $dir = dirname($dir);
+         } while ($dir && ($name = substr($name, 0, strrpos($name, ':'))));
+         return $list;
+     }
 
 }
