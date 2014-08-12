f90a90689bc79bcea675ff23ccd157b21431f0c4
Tomáš Voslař <tomas.voslar@webcook.cz>
2014-08-12 18:09:12 +0100

    Added custom layout path.

--- FrontendModule/presenters/BasePresenter.php
+++ FrontendModule/presenters/BasePresenter.php
@@ -542,5 +542,31 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
             "$dir/templates/$presenter.$this->view.phtml",
         );
     }
+    
+     /**
+      * Formats layout template file names.
+      * @return array
+      */
+     public function formatLayoutTemplateFiles()
+     {
+         $name = $this->getName();
+         $presenter = substr($name, strrpos(':' . $name, ':'));
+         $layout = $this->layout ? $this->layout : 'layout';
+         $dir = dirname($this->getReflection()->getFileName());
+         $dir = is_dir("$dir/templates") ? $dir : dirname($dir);
+         $list = array(
+             APP_DIR . "/templates/@$layout.latte",
+             "$dir/templates/$presenter/@$layout.latte",
+             "$dir/templates/$presenter.@$layout.latte",
+             "$dir/templates/$presenter/@$layout.phtml",
+             "$dir/templates/$presenter.@$layout.phtml",
+         );
+         do {
+             $list[] = "$dir/templates/@$layout.latte";
+             $list[] = "$dir/templates/@$layout.phtml";
+             $dir = dirname($dir);
+         } while ($dir && ($name = substr($name, 0, strrpos($name, ':'))));
+         return $list;
+     }
 
 }
