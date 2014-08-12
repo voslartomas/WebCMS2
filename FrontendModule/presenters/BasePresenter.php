f25c0d420f4e774565a036f26734aa76036d68a8
Tomáš Voslař <tomas.voslar@webcook.cz>
2014-08-12 17:59:55 +0100

    Added custom template path.

--- FrontendModule/presenters/BasePresenter.php
+++ FrontendModule/presenters/BasePresenter.php
@@ -496,5 +496,26 @@ class BasePresenter extends \WebCMS2\Common\BasePresenter
     {
         parent::flashMessage($this->translation[$text], $type);
     }
+    
+    /**
+    * Formats view template file names.
+    * @return array
+    */
+    public function formatTemplateFiles()
+    {
+        $name = $this->getName();
+        $presenter = substr($name, strrpos(':' . $name, ':'));
+        $dir = dirname($this->getReflection()->getFileName());
+        $dir = is_dir("$dir/templates") ? $dir : dirname($dir);
+        $appPath = APP_DIR . "/templates/" . lcfirst($presenter) . "-module/$presenter/$this->view.latte";
+        
+        return array(
+            $appPath,
+            "$dir/templates/$presenter/$this->view.latte",
+            "$dir/templates/$presenter.$this->view.latte",
+            "$dir/templates/$presenter/$this->view.phtml",
+            "$dir/templates/$presenter.$this->view.phtml",
+        );
+    }
 
 }
