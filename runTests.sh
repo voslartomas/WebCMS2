2eddeeba1c6505d1bfd58a4b92f98470edb6b8c9
Tomas Voslar <tomas.voslar@webcook.cz>
2014-08-30 00:52:24 +0100

    Refactored filesystem. Cleared JavaScript.

--- runTests.sh
+++ runTests.sh
@@ -5,10 +5,12 @@ mkdir tests/log
 mkdir upload
 mkdir thumbnails
 
-if [ "$1" = "" ]; then
+if [ "$1" = "coverage" ]; then
+    phpunit --coverage-clover=coverage.clover tests
+    wget https://scrutinizer-ci.com/ocular.phar
+    php ocular.phar code-coverage:upload --format=php-clover coverage.clover
     exit 0
 fi
 
-phpunit --coverage-clover=coverage.clover tests
-wget https://scrutinizer-ci.com/ocular.phar
-php ocular.phar code-coverage:upload --format=php-clover coverage.clover
+phpunit tests
+
